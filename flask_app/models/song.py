3e6541a986c4fd0a71477203a6ef00e27dfca7af
Rice Maxwell <patricemaxwell@yahoo.com>
2024-03-04 15:43:35 -0500

    added spotify intergarion with link to spotify

--- flask_app/models/song.py
+++ flask_app/models/song.py
@@ -11,6 +11,9 @@ class Song:
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
         self.user_id = data['user_id']
+        self.cover_link = data['cover_link']
+        self.track_link = data['track_link']
+        self.spot_id = data['spot_id']
 
     @staticmethod
     def new_song_validation(reg_form):
@@ -23,29 +26,26 @@ class Song:
             flash("The artist name must be greater than 2 characters")
             is_Valid = False
 
-        if not reg_form['length']:
-            flash("Length may cannot be empty")
-            is_Valid = False
-
         return is_Valid
 
     @classmethod
     def add_new_song(cls, data):
-        query = "INSERT INTO songs (title , artist , length , user_id) VALUE (%(title)s, %(artist)s , %(length)s, %(user_id)s);"
+        query = "INSERT INTO songs (title , artist , length , user_id , cover_link, track_link, spot_id) VALUE (%(title)s, %(artist)s , %(length)s, %(user_id)s , %(cover_link)s, %(track_link)s , %(spot_id)s);"
 
-        connectToMySQL('roving_rave_schema').query_db(query, data)
+        connectToMySQL('roving_rave_spotify_schema').query_db(query, data)
 
         query1 = "SELECT id FROM songs \
             WHERE title = %(title)s \
                 ORDER BY created_at DESC \
                     LIMIT 1"
 
-        song_id = connectToMySQL('roving_rave_schema').query_db(query1, data)
+        song_id = connectToMySQL(
+            'roving_rave_spotify_schema').query_db(query1, data)
 
         data['song_id'] = song_id[0]['id']
 
         query2 = "INSERT INTO crowd_queue (song_id , user_id) VALUE ( %(song_id)s , %(user_id)s) "
-        connectToMySQL('roving_rave_schema').query_db(query2, data)
+        connectToMySQL('roving_rave_spotify_schema').query_db(query2, data)
 
     @classmethod
     def get_all_crowd_songs(cls):
@@ -53,7 +53,7 @@ class Song:
             LEFT JOIN songs ON song_id = songs.id \
              LEFT JOIN users ON  crowd_queue.user_id = users.id ;"
 
-        results = connectToMySQL('roving_rave_schema').query_db(query)
+        results = connectToMySQL('roving_rave_spotify_schema').query_db(query)
 
         return results
 
@@ -61,23 +61,23 @@ class Song:
     def delete_song(cls, data):
 
         query = "DELETE FROM songs WHERE id = %(id)s;"
-        connectToMySQL('roving_rave_schema').query_db(query, data)
+        connectToMySQL('roving_rave_spotify_schema').query_db(query, data)
 
     @classmethod
     def delete_song_from_crowd(cls, data):
         query = "DELETE FROM crowd_queue WHERE id = %(crowd_id)s;"
-        connectToMySQL('roving_rave_schema').query_db(query, data)
+        connectToMySQL('roving_rave_spotify_schema').query_db(query, data)
 
     @classmethod
     def delete_song_from_queue(cls, data):
         query = "DELETE FROM queues WHERE id = %(id)s;"
-        connectToMySQL('roving_rave_schema').query_db(query, data)
+        connectToMySQL('roving_rave_spotify_schema').query_db(query, data)
 
     @classmethod
     def add_to_queue(cls, data):
         query = "INSERT INTO queues (user_id ,song_id) VALUES (%(user_id)s , %(id)s );"
 
-        connectToMySQL('roving_rave_schema').query_db(query, data)
+        connectToMySQL('roving_rave_spotify_schema').query_db(query, data)
 
     @classmethod
     def get_queue(cls):
@@ -85,7 +85,7 @@ class Song:
         left join songs ON song_id = songs.id \
         left JOIN users ON queues.user_id = users.id;"
 
-        results = connectToMySQL('roving_rave_schema').query_db(query)
+        results = connectToMySQL('roving_rave_spotify_schema').query_db(query)
 
         return results
 
@@ -96,7 +96,8 @@ class Song:
             LEFT JOIN users ON queues.user_id = users.id \
             WHERE queues.id = %(id)s ; "
 
-        results = connectToMySQL('roving_rave_schema').query_db(query, data)
+        results = connectToMySQL(
+            'roving_rave_spotify_schema').query_db(query, data)
 
         return results
 
@@ -104,7 +105,7 @@ class Song:
     def add_to_current(cls, data):
         query = "INSERT INTO current_song (user_id ,song_id) VALUES (%(user_id)s , %(song_id)s );"
 
-        connectToMySQL('roving_rave_schema').query_db(query, data)
+        connectToMySQL('roving_rave_spotify_schema').query_db(query, data)
 
     @classmethod
     def get_current(cls):
@@ -114,6 +115,14 @@ class Song:
         order by current_song.created_at desc \
         limit 1;"
 
-        results = connectToMySQL('roving_rave_schema').query_db(query)
+        results = connectToMySQL('roving_rave_spotify_schema').query_db(query)
+        if len(results) < 1:
+            results = [{
 
+                'title': "No song playing",
+                'artist': "N/A",
+                'length': "00:00",
+                'cover_link': "../static/img/no-img-avail.png"
+            }]
+        print(results)
         return results
